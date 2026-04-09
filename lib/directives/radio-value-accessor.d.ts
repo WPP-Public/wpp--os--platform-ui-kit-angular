3ac8c03e5cfec9d4a503e7f94470e44a91dffabd
nikita-ryzhov-wpp <nikita.ryzhov@wpp.com>
2026-04-09 13:13:14 +0200

    Release v4.0.0 (#5)
    * Release v4.0.0

--- lib/directives/radio-value-accessor.d.ts
+++ lib/directives/radio-value-accessor.d.ts
@@ -4,5 +4,5 @@ import * as i0 from "@angular/core";
 export declare class RadioValueAccessor extends ValueAccessor {
     constructor(el: ElementRef);
     static ɵfac: i0.ɵɵFactoryDeclaration<RadioValueAccessor, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<RadioValueAccessor, "wpp-radio-group-v3-5-0-v3-5-0, wpp-segmented-control-v3-5-0-v3-5-0", never, {}, {}, never, never, true, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<RadioValueAccessor, "wpp-radio-group-v4-0-0-v4-0-0, wpp-segmented-control-v4-0-0-v4-0-0", never, {}, {}, never, never, true, never>;
 }
